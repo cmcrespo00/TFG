3624998e9ed7bb999cde42a443fd3ad99fd1867a
cmcrespo00 <82025386+cmcrespo00@users.noreply.github.com>
2023-09-06 21:28:52 +0200

    Delete script.sh

--- script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#Ejecutar el programa para obtener los datos de Temperatura y Humedad
-python sensorTH.py &
-
-#Ejecutar el programa para obtener los datos de la camara
-python sensorCamara.py &
-
-echo "Ejecutando programas"
